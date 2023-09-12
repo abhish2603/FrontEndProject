d39f7726aaf9b30fb96c593f870c85fba440a356
abhish2603 <63105072+abhish2603@users.noreply.github.com>
2023-09-12 17:51:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const fetchPrice = async(ctype) =>{
     const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
     console.log(r.data.coin.price);
     const price = r.data.coin.price;
-    const volume = r.data.volume;
-    const change = r.data.coin.priceChangeid;
+    const volume = r.data.coin.volume;
+    const change = r.data.coin.priceChange1d;
     const base = r.data.coin.name;
     const target = 'USD';
 
